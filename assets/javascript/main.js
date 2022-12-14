dbf7602e0e419566213a2981b3a454b0cc5a4261
Matheus <matheusmm2019@gmail.com>
2022-12-14 01:58:52 -0300

    Primeiras requisições

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,24 @@
-console.log('Fooi')
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?effect=${offset}&limit=${limit}`
+
+fetch(url)
+.then((response) => {return response.json()})
+.then( (jsonBody)=>{ console.log(jsonBody)})
+.catch((error)=> {console.log(error)})
+
+
+// fetch(url)
+// .then(function(response){
+//     response
+//         .json()
+//         .then(function(responseBody){
+//             console.log(responseBody)
+//         })
+// })
+// .catch(function(error){
+//     console.log(error)
+// })
+// .finally(function(){
+//     console.log("Operação concluida")
+// })
