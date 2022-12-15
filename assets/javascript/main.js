b4bf24afe5d416552082dcc133e033da9ee8d3f9
Matheus <matheusmm2019@gmail.com>
2022-12-14 23:37:52 -0300

    Começando as integrações com a api

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,9 +2,34 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?effect=${offset}&limit=${limit}`
 
+function convertPokemonToLi(pokemon){
+    return `
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png">
+            alt="${pokemon.name}"
+        </div>
+    </li>
+`
+}
+
 fetch(url)
-.then((response) => {return response.json()})
-.then( (jsonBody)=>{ console.log(jsonBody)})
+.then((response) => response.json())
+.then( (jsonBody)=> jsonBody.results)
+.then( (pokemonsList)=>{ 
+    for (let i = 0; i < pokemonsList.length; i++) {
+        const pokemon = pokemonsList[i];
+        console.log(convertPokemonToLi(pokemon))
+        
+    }
+})
 .catch((error)=> {console.log(error)})
 
 
